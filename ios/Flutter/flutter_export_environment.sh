c7e8e9d5d54130284620e09a806f8e53d2ac11f2
masterUNG <phrombutr@gmail.com>
2020-10-01 10:46:04 +0700

    update

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/masterung/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/masterung/MyWorkSpace/ConsultFlutter/send"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/masterung/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.4.1"
+export "FLUTTER_BUILD_NUMBER=2.4.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
